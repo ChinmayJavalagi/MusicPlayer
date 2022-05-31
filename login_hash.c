5ff8d3a9991d2a27097548a4fef48db005b12244
Chinmay01234 <chinmayjavalagi@gmail.com>
2022-05-31 23:17:03 +0530

    Updated

--- login_hash.c
+++ login_hash.c
@@ -1,4 +1,6 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #define size 11
 struct node
 {
@@ -11,33 +13,31 @@ struct node *chain[size];
 void init()
 {
     int i;
-    for(i = 0; i < size; i++)
+    for (i = 0; i < size; i++)
         chain[i] = NULL;
 }
-
-
 
 void signup(long long int phone)
 {
 
-    //create a newnode with value
+    // create a newnode with value
     struct node *newNode = malloc(sizeof(struct node));
     printf("Password:");
-    scanf("%s",newNode->data);
+    scanf("%s", newNode->data);
     newNode->next = NULL;
 
-    //calculate hash key
+    // calculate hash key
     int key = phone % size;
 
-    //check if chain[key] is empty
-    if(chain[key] == NULL)
+    // check if chain[key] is empty
+    if (chain[key] == NULL)
         chain[key] = newNode;
-    //collision
+    // collision
     else
     {
-        //add the node at the end of chain[key].
+        // add the node at the end of chain[key].
         struct node *temp = chain[key];
-        while(temp->next)
+        while (temp->next)
         {
             temp = temp->next;
         }
@@ -46,48 +46,45 @@ void signup(long long int phone)
     }
     printf("Sign up completed");
 }
-void hash(long long int phone,char password[20])
+void hash(long long int phone, char password[20])
 {
     int key;
-    key=phone%size;
-    struct node * cur=chain[key];
-    while(cur!=NULL)
+    key = phone % size;
+    struct node *cur = chain[key];
+    while (cur != NULL)
     {
-        if(strcmp(cur->data,password)==0)
+        if (strcmp(cur->data, password) == 0)
         {
             printf("logged In");
             break;
         }
         else
         {
-            cur=cur->next;
+            cur = cur->next;
         }
     }
-
 }
 void login(long long int phone)
 {
 
     char password[20];
     printf("Phone number ");
-    scanf("%lld",&phone);
+    scanf("%lld", &phone);
     printf("Password: ");
-    scanf("%s",password);
-    hash(phone,password);
-
-
+    scanf("%s", password);
+    hash(phone, password);
 }
 void print()
 {
     int i;
 
-    for(i = 0; i < size; i++)
+    for (i = 0; i < size; i++)
     {
         struct node *temp = chain[i];
-        printf("chain[%d]-->",i);
-        while(temp)
+        printf("chain[%d]-->", i);
+        while (temp)
         {
-            printf("%s -->",temp->data);
+            printf("%s -->", temp->data);
             temp = temp->next;
         }
         printf("NULL\n");

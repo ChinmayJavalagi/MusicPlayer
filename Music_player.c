9fb155dd025e2324c198099ea35c5a8321cbf08e
Chinmay01234 <chinmayjavalagi@gmail.com>
2022-06-01 05:42:22 +0530

    update

--- Music_player.c
+++ Music_player.c
@@ -2,27 +2,33 @@
 #include <stdlib.h>
 #include <string.h>
 #include "temp.h"
-typedef struct num
-{
-    int data;
-    struct num *next;
-    struct num *prev;
 
-} * node;
+typedef struct playlist
+{
+    char song[20];
+    struct playlist *next;
+    struct playlist *prev;
+
+}*node;
+
 typedef struct mobile
 {
     long long int phone;
     struct node1 *next1;
 } * node1;
-/*node create_node()
+
+
+node create_node()
 {
     node temp;int n;
-    temp=(node)malloc(sizeof(struct num));
+    temp=(node)malloc(sizeof(struct playlist));
     printf("enter the number:");
-    scanf("%d",&temp->data);
+    scanf("%s",&temp->song);
     temp->next=NULL;
     temp->prev=NULL;
-}*/
+}
+
+
 node1 create_node1(long long int phone)
 {
     node1 temp1;
@@ -31,7 +37,7 @@ node1 create_node1(long long int phone)
     temp1->next1 = NULL;
 }
 
-/*node insert_end(node temp,node head)
+node insert_end(node temp,node head)
 {
     node cur=head;
     if(head==NULL)
@@ -45,7 +51,8 @@ node1 create_node1(long long int phone)
     cur->next=temp;
     temp->prev=cur;
     return head;
-}*/
+}
+
 /*void display(node head)
 {
     node cur=head;
@@ -114,6 +121,7 @@ node1 create_node1(long long int phone)
         printf("Invalid no\n");
 
 }*/
+
 node1 insert_beg(node1 temp1, node1 head1)
 {
     if (head1 == NULL)
@@ -125,6 +133,7 @@ node1 insert_beg(node1 temp1, node1 head1)
     head1 = temp1;
     return head1;
 }
+
 void find_phone(long long int phone, node1 head1)
 {
 
@@ -170,23 +179,122 @@ void user(node1 head1)
         break;
     }
 }
+
+
+node create_playlist()
+{
+    node temp;int n;
+    temp=(node)malloc(sizeof(struct playlist));
+    printf("Playlist name: ");
+    scanf("%s",&temp->song);
+    temp->next=NULL;
+    temp->prev=NULL;
+}
+
+void count_nodes(node head)          //Total Songs
+{
+    int i=0;
+    node cur = head->next;
+    while (cur->next!=NULL)
+    {
+        cur=cur->next;
+        i++;
+    }
+    printf("Total songs-->%d",i);
+}
+
+void search1(node head)              //Searching a song
+{
+    char song[100];
+    node cur = head->next;
+    printf("\n\a\a\a\aEnter song To be Searched- ");
+    scanf("%s",&song);
+    int flag=0;
+
+    while(cur!=NULL)
+    {
+        if(strcmp(cur->song,song)==0)
+        {
+            printf("\n\a\a\a\a#Song Found\n");
+            flag++;
+            break;
+        }
+        else
+        {
+            cur=cur->next;
+        }
+    }
+    if(flag==0)
+    {
+        printf("\n\a\a\a\a#Song Not found");
+    }
+}
+
+void play(node head)                //Play a song
+{
+    char song[100];
+    node cur = head->next;
+    printlist(head);
+    printf("\n\a\a\a\aChoose song you wish to play- ");
+    scanf("%s",song);
+    int flag=0;
+
+    while(cur!=NULL)
+    {
+        if(strcmp(cur->song,song)==0)
+        {
+            printf("\n\a\a\a\a=>Now Playing......%s",song);
+            flag++;
+            push(song);
+            break;
+        }
+        else
+        {
+            cur=cur->next;
+        }
+    }
+    if(flag==0)
+    {
+        printf("\n\a\a\a\a#Song Not found");
+    }
+}
+
+void printlist(node head)                        //Print all the songs in a playlist
+{
+    node cur = head->next;
+    printf("\nPlaylist Name- %s",head->song);
+    while(cur->next!=NULL)
+    {
+        printf("%s",cur->song);
+        cur=cur->next;
+    }
+    printf("%s",cur->song);
+}
+
 void main()
 {
     // node temp,head=NULL;int ch;int n,x;char name[20];
     node1 head1 = NULL;
     init();      // initializing chain to NULL
     user(head1); // Signup or login
-    /*while(1)
+
+    node temp,head=NULL;int ch;int n,x;
+    node headm[10];
+    printf("Enter the name of your playlist: ");
+    temp = create_playlist();
+    headm[0] = insert_end(temp,head);
+
+    while(1)
     {
-        printf(" 1.Add a new song\n 2.delete song\n 3.display the entire playlist\n 4.Total songs\n 5.search song\n 6.recently played track\n 7.sorted playlist\n 8.create a new playlist");
+        printf("\n1.Add New Song\n 2.Delete Song\n3.Display Entered Playlist\n4.Total Songs\n5.Search Song\n6.Play Song\n7.Recently Played List\n8.Last Played\n9.Sorted playlist\n");
         scanf("%d",&ch);
         switch(ch)
         {
-            case 1:  printf("Which linked list?:\n") ;
+            case 1: printf("Which linked list?:\n") ;
                     scanf("%d",&x);
                     temp=create_node();
                     headm[x]=insert_end(temp,headm[x]);
-                      break;
+                    break;
             case 2: printf("Which linked list?:\n") ;
                     scanf("%d",&x);
                     temp=create_node();
@@ -213,8 +321,8 @@ void main()
                     scanf("%d",&x);
                     display(headm[x]);
                     break;
-            default: exit(0);
 
+            default: exit(0);
         }
-    }*/
+    }
 }
